7d73deb78000f4f74141febe448dfda11109f5b5
Michael Simpson <mds6@pdx.edu>
2015-04-27 23:46:26 -0700

    checkpoint

--- program6.s
+++ program6.s
@@ -11,16 +11,16 @@ f:
     cmpl   %ecx,   %eax           # Compare the two lengths
     je    equal                   # Jump past singularly empty comparison if equal
     cmpl   $0,     %ecx           # See if the first array is empty
-    je    fEmpty                  # Go to special case handler
+    je    fEmpty                  # First array is empty
     cmpl   $0,     %eax           # See if the second array is empty
-    je    sEmpty                  # Go to special case handler
-    jmp   rest
+    je    sEmpty                  # Second array is empty
+    jmp   rest                    # Array length unequal and both arrays non empty
 ### This is where your code ends ...
 
 equal:
-    cmpl   $0,     %eax
-    je    done
-
+    cmpl   $0,     %eax           # If one array is empty
+    je    done                    # Then both arrays are empty
+    jmp   rest                    # Arrays are not empty
 fEmpty:
 
     jmp done
@@ -33,20 +33,38 @@ rest:
     leaq 4(%rdi),  %r9            # Copy a pointer to the first element of the first array 
     movslq %eax,   %rdx           # Sign extend the length of the second array for indexed access
     leaq  (%rsi,   %rdx, 4), %r10 # Copy a pointer to the last element of the second array
-#    movl  (%r9),   %edx           # Copy the value in the first element of the first array
-#    movl  (%r10),  %ecx           # Copy the value of the last element of the second array
-#    movl   %edx,  (%r10)          # Move the first element of the first array to the last element of the second 
-#loop:
-#    cmpl   $1,     %eax
-#    jng   rot
-#    subq   $4,     %r10
-#    decl   %eax
-#    movl  (%r10),  %edx
-#    movl   %ecx,   (%r10)
-#    jmp   loop
+    movl  (%r9),   %edx           # Copy the value in the first element of the first array
+    movl  (%r10),  %ecx           # Copy the value of the last element of the second array
+    movl   %edx,  (%r10)          # Move the first element of the first array to the last element of the second 
+loop:
+    cmpl   $1,     %eax           # Compare the remaining length to one
+    jng   rot                     # Jump to next phase in rotation if length is less than or equal to one
+    subq   $4,     %r10           # Move the index of the second array back one
+    decl   %eax                   # Decrement the length of the second array
+    movl  (%r10),  %edx           # Copy the current indexed value
+    movl   %ecx,  (%r10)          # Replace the currrent indexed value with the value previously at "[i+1]"
+    movl   %edx,   %ecx           # Copy the old current to be placed in a new index
+    jmp   loop                    # Loop back to check the length
     
 rot:
-   # movl   %edx , (%r10)
-    #movl   %edx,  %eax
+    movl  (%rdi),  %eax           # Copy the length of the first array
+    movslq %eax,   %rdx           # Sign extend the length of the array for indexed access
+    leaq  (%rdi,   %rdx, 4), %r10 # Point to the last index in the first array 
+    movl  (%r10),  %edx           # Copy the value at that index
+    movl   %ecx,  (%r10)          # Replace the value at that index with the rotated value
+otLoop: 
+    cmpl   $1,     %eax           # Compare the length of the first array to one
+    jng   houseKeep               # If the length is less than or equal to one
+    subq   $4,     %r10           # Move the index of the first array back one
+    decl   %eax                   # Decrement the length of the first array
+    movl  (%r10),  %ecx           # Copy the current indexed value
+    movl   %edx,  (%r10)          # Replace the current indexed value with the value previously at "[i+1]"
+    movl   %ecx,   %edx           # Copy the old current to be placed in a new index
+    jmp   otLoop                  # Loop back to check the length
+
+houseKeep:
+    movl  (%rdi),  %eax
+    movl  (%rsi),  %ecx
+    addl   %ecx,   %eax
 done:
     ret
